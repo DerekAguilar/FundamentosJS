8390040bba14eddcb6748994d9ee305aa0a4f487
Derek Eduardo Aguilar Reyes <aguilarrde753@gmail.com>
2023-09-11 15:46:42 -0600

    04 Operadores

---
+++ operadores.js
@@ -0,0 +1,37 @@
+
+
+
+// Operadores Matemáticos
+// + - * / % **
+// += -= *= /= %= **=
+console.log(10 + 10); // Suma
+console.log(10 - 10); // Resta
+console.log(10 * 10); // Multiplicación
+console.log(10 / 10); // División
+console.log(10 % 10); // Módulo o residuo
+console.log(10 ** 10); // Potencia
+
+let num = 15;
+console.log(num**=5);
+console.log(num+=5); // num = num + 5
+console.log(num-=5); // num = num - 5
+console.log(num*=5); // num = num * 5
+console.log(num/=5); // num = num / 5
+console.log(num%=5); // num = num % 5
+
+// Operadores booleanos
+// > < >= <= !== ===
+console.log(1 > 2);
+console.log(1 < 2);
+console.log(1 >= 2);
+console.log(1 <= 2);
+console.log(1 !== 2);
+console.log(1 === 2);
+
+// Operadores lógicos
+// and &&
+console.log(true && true);
+// or ||
+console.log(true || false);
+// not !
+console.log(!true);
